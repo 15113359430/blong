c360251d9c10ed11b9023a5817c2f59cfefba493
licun <354142362@qq.com>
2019-04-19 09:38:23 +0800

    blong

---
+++ resources/views/admin/user/pass.blade.php
@@ -0,0 +1,70 @@
+@extends('admin.layout.layout')
+@section('center')
+<body>
+<div class="panel admin-panel">
+  <div class="panel-head"><strong><span class="icon-key"></span> 修改会员密码</strong></div>
+  <div class="body-content">
+    <form method="post" id="form" class="form-x" action="{{url('admin/pass')}}">
+     
+     {{csrf_field()}}
+      <div class="form-group">
+        <div class="label">
+          <label for="sitename">管理员帐号：</label>
+        </div>
+        <div class="field">
+          <label style="line-height:33px;">
+           {{session('name')}}
+          </label>
+        </div>
+      </div>      
+      <div class="form-group">
+        <div class="label">
+          <label for="sitename">原始密码：</label>
+        </div>
+        <div class="field">
+          <input type="password" class="input w50" id="mpass" datatype="s4-10" ajaxurl="{{url('admin/charkpass')}}" name="password" size="50" placeholder="请输入原始密码" data-validate="required:请输入原始密码" />       
+        </div>
+      </div>      
+      <div class="form-group">
+        <div class="label">
+          <label for="sitename">新密码：</label>
+        </div>
+        <div class="field">
+          <input type="password" class="input w50" name="newpass" datatype="s4-10" size="50" placeholder="请输入新密码" data-validate="required:请输入新密码,length#>=5:新密码不能小于5位" />         
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="label">
+          <label for="sitename">确认新密码：</label>
+        </div>
+        <div class="field">
+          <input type="password" class="input w50" name="renewpass" datatype="s4-10" recheck="newpass" size="50" placeholder="请再次输入新密码" data-validate="required:请再次输入新密码,repeat#newpass:两次输入的密码不一致" />          
+        </div>
+      </div>
+      
+      <div class="form-group">
+        <div class="label">
+          <label></label>
+        </div>
+        <div class="field">
+          <button class="button bg-main icon-check-square-o" type="submit"> 提交</button>   
+        </div>
+      </div>      
+    </form>
+  </div>
+</div>
+<script>
+$('#form').Validform({
+    tiptype:4,
+    ajaxPost:true,
+    callback:function(res){
+        if(res.status == 'success'){
+            window.parent.location.href="{{url('admin/logout')}}";
+        }else{
+            layer.msg(res.msg,{icon:2});
+        }
+    }
+})
+</script>
+</body>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function(){
+	return view('welcome');
+});
+//后台登录
+Route::get('admin/login', 'Admin\LoginController@index');
+//后台验证
+Route::post('admin/login', 'Admin\LoginController@login');
+//退出登录
+Route::get('admin/logout','Admin\LoginController@logout');
+//后台首页
+Route::get('admin/index','Admin\IndexController@index')->middleware('login');
+
+//网站设置
+Route::get('admin/system','Admin\SystemController@index');
+
+//banner
+Route::resource('admin/image','Admin\ImageController');
+
+//留言管理
+Route::get('admin/comment','Admin\CommentController@index');
+
+//文章管理
+Route::resource('admin/article','Admin\AriticeController');
+
+//分类管理
+Route::resource('admin/cat','Admin\CatController');
+
+//管理员密码修改
+Route::match(['get','post'],'admin/pass','Admin\UserController@pass');
+
+//用户列表
+Route::get('admin/user','Admin\UserController@index');
+
+//通过ajax匹配原密码
+Route::post('admin/charkpass','Admin\LoginController@charkpass');
